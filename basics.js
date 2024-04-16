1c8937e7b1637ee3e2248fa83ca45d4abd1dcd07
BhuvanMM <124192779+BhuvanMM@users.noreply.github.com>
2024-04-16 02:12:39 +0000

    basics of data types and variables

---
+++ basics.js
@@ -0,0 +1,14 @@
+let name="Bhuvan"
+const password=1234
+let email="bhuvan@gmail.com"
+person_name="bopanna"
+console.table([name,password,email,person_name])
+
+// datatypes
+/*
+1.number
+2.null=> not a valid value
+3.undefined=>undefined
+4.bigint
+5.string
+*/
