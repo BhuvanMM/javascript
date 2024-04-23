6c3055534247f5cfafd87a77612dad20237d0131
BhuvanMM <124192779+BhuvanMM@users.noreply.github.com>
2024-04-23 11:07:40 +0000

    data type conversion

--- conversion.js
+++ conversion.js
@@ -2,8 +2,22 @@ let num="33"
 let numCon = Number(num)
 console.log(numCon)
 
+
+let isLoggedIn=0; // 1 =>true and 0=>false
+let x=Boolean(isLoggedIn);
+console.log(x);
 //1. "33" => 33
 //2. "33abs"=> NaN
 //3. true=>1 false=>0
 //4. 1=>true 0=>false
 //5. "" =>false "bhuvan"=>true
+
+//demo for "" and "value"
+
+let str="";
+let a=Boolean(str);
+console.log(a);//false
+
+let str1="bhuvan"
+let b=Boolean(str1);
+console.log(b); //true
