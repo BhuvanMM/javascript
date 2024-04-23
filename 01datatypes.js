cbc72b5e4f60ccf24455f19e6380a9f44b7e4c9f
BhuvanMM <124192779+BhuvanMM@users.noreply.github.com>
2024-04-23 11:01:21 +0000

    Data types important points

---
+++ 01datatypes.js
@@ -0,0 +1,13 @@
+//null => standalone value
+//undefined => undefined 
+
+// <---------- very important -------->
+/* 
+    type of null => object
+    type of undefined => undefined 
+*/
+
+/* 
+1. boolean =>true/false
+2.string =>"" //use double quotes
+*/ 
