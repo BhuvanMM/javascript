7a84eafbdedfdb719b0ade8b4376ef68adbc7684
BhuvanMM <124192779+BhuvanMM@users.noreply.github.com>
2024-04-23 12:14:30 +0000

    math in js

---
+++ math.js
@@ -0,0 +1,11 @@
+//mathematics
+console.log(Math.round(4.8));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.6));
+
+//random function most advanced way to use
+const min=10
+const max=20
+
+const ans=(Math.random() * (max-min + 1)) + min
+console.log(Math.floor(ans));
